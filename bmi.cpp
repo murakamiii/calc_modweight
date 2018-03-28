84fcc77dfa251773f633267ef5ba041b2a824866
tset <tset@example.com>
2018-03-29 01:51:48 +0900

    [WIP] TODO記述

--- bmi.cpp
+++ bmi.cpp
@@ -30,12 +30,14 @@ class BodyData
         }
         double maxWeight()
         {
+            // TODO: 相対的な筋力・耐久値の高さに対して変動する
             if(!guardLowerIntMaxSqrt(height)) { return INT_MAX; }
             double d = (double)height * (double)height * 24.0 / 10000.0;
             return clampWeightRange((int)d);
         }
         double meanWeight() 
         {
+            // TODO: 相対的な筋力・耐久値の高さに対して変動する
             double d = (0.5 * minWeight() + 0.5 * maxWeight() ) ;
             return clampWeightRange(d);
         }
@@ -44,6 +46,17 @@ class BodyData
             double coeff = (weight - meanWeight()) / (maxWeight() - meanWeight());
             double cosValue = cos(coeff * M_PI) * -1;
             return std::clamp(cosValue, 0.0, 1.0);
+        }
+        int adjustWeight()
+        {
+            // TODO:
+            // 与えられたdeltaに対して 痩せ&減量 || 肥満&増量ならdeltaが区間[0,delta]の一様分布で選びなおし
+            // 逆の関係ならdeltaが二倍になる 
+
+            // 元の体重 * (100 + delta) / (100 + (増量 ? (+1) : (-1)))
+
+            // TODO: 0以下なら1を返す
+            return 1; 
         }
         
         
