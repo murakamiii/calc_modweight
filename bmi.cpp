b39e3aa3a1c7d1e46e7cb7e205dcce0331a736b5
tset <tset@example.com>
2018-04-02 01:04:42 +0900

    補正込みの体重変動まで

--- bmi.cpp
+++ bmi.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <climits>
 #include <cmath>  
+#include <random>
 
 int clampWeightRange(int intValue) 
 {
@@ -47,16 +48,29 @@ class BodyData
             double cosValue = cos(coeff * M_PI) * -1;
             return std::clamp(cosValue, 0.0, 1.0);
         }
-        int adjustWeight()
+        int adjustDelta(int delta)
         {
-            // TODO:
-            // 与えられたdeltaに対して 痩せ&減量 || 肥満&増量ならdeltaが区間[0,delta]の一様分布で選びなおし
+            // (1- prob)の確率で補正なしでreturn
+            std::random_device seed_gen;
+            std::mt19937 engine(seed_gen());
+            std::bernoulli_distribution dist(prob());
+            if(!dist(engine)) { return delta; }
+
+            // 与えられたdeltaに対して 痩せ&減量 || 肥満&増量ならdeltaは0
             // 逆の関係ならdeltaが二倍になる 
+            if(std::signbit((double)delta) && std::signbit(weight - meanWeight())) {
+                return 0;
+            } else {
+                return delta * 2;
+            }
+        }
+        int adjustWeight(int delta) {
+            // 本家仕様では“元の体重 * (100 + delta) / (100 + (増量 ? (+1) : (-1)))」”
+            double diff = (100.0 + adjustDelta(delta)) / 100.0;
+            double adjusted = weight * diff;
 
-            // 元の体重 * (100 + delta) / (100 + (増量 ? (+1) : (-1)))
-
-            // TODO: 0以下なら1を返す
-            return 1; 
+            // TODO: 例えば身長48cm,体重1kgだと身長を伸ばさない限り体重が増えないので対策する必要がある
+            return clampWeightRange((int)adjusted);
         }
         
         
@@ -67,10 +81,13 @@ class BodyData
 
 int main() {
     std::cout << "weightdiff" << std::endl;
-    BodyData bdata(22, 181);
+    BodyData bdata(1, 46);
     std::cout << "Min " << bdata.minWeight() << std::endl;
     std::cout << "Max " << bdata.maxWeight() << std::endl;
     std::cout << "Mean " << bdata.meanWeight() << std::endl;
     std::cout << "prob " << bdata.prob() << std::endl;
+    std::cout << "adjust " << bdata.adjustDelta(10) << std::endl;
+    std::cout << "adjust " << bdata.adjustWeight(10) << std::endl;
+
     return 0;
 }
